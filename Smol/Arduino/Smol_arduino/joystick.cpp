3e98e364b345959fc99fde4a7374b23485913e04
LauriGui <laurianne.guindon@polymtl.ca>
2023-03-30 15:18:30 -0400

    revert joystick and gyro

--- Smol/Arduino/Smol_arduino/joystick.cpp
+++ Smol/Arduino/Smol_arduino/joystick.cpp
@@ -10,88 +10,93 @@ int right_strength;
 float strength_scaler = 100;
 
 enum {
-  FIRST,
-  SECOND,
-  THIRD,
-  FOURTH,
-  FRONT,
-  RIGHT,
-  LEFT,
-  BACK
+    FIRST,
+    SECOND,
+    THIRD,
+    FOURTH,
+    FRONT,
+    RIGHT,
+    LEFT,
+    BACK
 };
 
 int getCadrans(int angle) {
-  // TODO: ajuster pour le tourner 360 degrees
-  // TODO: ajuster les angles de 90 et 270
-  if (angle < 90 && angle > 10) return FIRST;                                       // 0
-  if (angle > 90 && angle < 170) return SECOND;                                     // 1
-  if (angle > 190 && angle < 270) return THIRD;                                     // 2
-  if (angle > 270 && angle < 350) return FOURTH;                                    // 3
-  if (angle == 90) return FRONT;                                                    // 4
-  if ((angle >= 0 && angle <= 10) || (angle <= 360 && angle >= 350)) return RIGHT;  // 5
-  if (angle >= 170 && angle <= 190) return LEFT;                                    // 6
-  if (angle == 270) return BACK;                                                    // 7
+    // TODO: ajuster pour le tourner 360 degrees
+    // TODO: ajuster les angles de 90 et 270
+    if (angle < 90 && angle > 10) return FIRST;                                       // 0
+    if (angle > 90 && angle < 170) return SECOND;                                     // 1
+    if (angle > 190 && angle < 270) return THIRD;                                     // 2
+    if (angle > 270 && angle < 350) return FOURTH;                                    // 3
+    if (angle == 90) return FRONT;                                                    // 4
+    if ((angle >= 0 && angle <= 10) || (angle <= 360 && angle >= 350)) return RIGHT;  // 5
+    if (angle >= 170 && angle <= 190) return LEFT;                                    // 6
+    if (angle == 270) return BACK;                                                    // 7
 
-  return 100;  // Erreur
+    return 100;  // Erreur
 }
 
 void advance_joystick(String* msg, float* angle_radi, float* sin_res, float* cos_res, float* strength, int* cad) {
-  *strength = getValue(*msg, ',', STRENGTH_POSITION).toInt();
+    *strength = getValue(*msg, ',', STRENGTH_POSITION).toInt();
+    // Calcul d'angles: https://arduinogetstarted.com/reference/arduino-sin ... adapter pour cos
+    String angle = getValue(*msg, ',', ANGLE_POSITION);
+    *angle_radi = angle.toInt() * M_PI / 180;
+    *sin_res = sin(*angle_radi);
+    *cos_res = cos(*angle_radi);
+    *cad = getCadrans(angle.toInt());
 
-  if (*strength == 0) {
-      left_strength = right_strength = 0;
-      //cane.stop(0);
-      stop();
-      return;
-  }
+    if (*strength == 0) {
+        left_strength = right_strength = 0;
+        stop();
+        return;
+    }
 
-  // Calcul d'angles: https://arduinogetstarted.com/reference/arduino-sin ... adapter pour cos
-  String angle = getValue(*msg, ',', ANGLE_POSITION);
-  *angle_radi = angle.toInt() * M_PI / 180;
-  *sin_res = sin(*angle_radi);
-  *cos_res = cos(*angle_radi);
-  *cad = getCadrans(angle.toInt());
+    float speed_scaler = (*strength / strength_scaler);
 
-  float speed_scaler = (*strength / strength_scaler);
-
-  switch (*cad) {
+    switch (*cad) {
     case FIRST:
     case FOURTH:
-      left_strength = vitesseMinJoystick + vitesseDifference * speed_scaler;
-      right_strength = vitesseMinJoystick + vitesseDifference * abs(*sin_res) * speed_scaler;
-      break;
+        left_strength = vitesseMinJoystick + vitesseDifference * speed_scaler;
+        right_strength = vitesseMinJoystick + vitesseDifference * abs(*sin_res) * speed_scaler;
+        break;
     case SECOND:
     case THIRD:
-      right_strength = vitesseMinJoystick + vitesseDifference * speed_scaler;
-      left_strength = vitesseMinJoystick + vitesseDifference * abs(*sin_res) * speed_scaler;
-      break;
+        right_strength = vitesseMinJoystick + vitesseDifference * speed_scaler;
+        left_strength = vitesseMinJoystick + vitesseDifference * abs(*sin_res) * speed_scaler;
+        break;
     case BACK:
     case FRONT:
     case LEFT:
     case RIGHT:
-      left_strength = right_strength = vitesseMinJoystick + vitesseDifference * speed_scaler;
-      break;
-  }
+        left_strength = right_strength = vitesseMinJoystick + vitesseDifference * speed_scaler;
+        break;
+    }
 
-  //cane.start(2000);
-  switch (*cad) {
+    switch (*cad) {
     case FIRST:
-    case SECOND:
-    case FRONT:
-      advance(right_strength, left_strength);
-      break;
+        advance(right_strength, left_strength);
+        break;
     case FOURTH:
+        back_off(right_strength, left_strength);
+        break;
+    case SECOND:
+        advance(right_strength, left_strength);
+        break;
     case THIRD:
+        back_off(right_strength, left_strength);
+        break;
     case BACK:
-      back_off(right_strength, left_strength);
-      break;
+        back_off(right_strength, left_strength);
+        break;
+    case FRONT:
+        advance(right_strength, left_strength);
+        break;
     case LEFT:
-      turn_R_360(right_strength, left_strength);  // TODO: Verify why in Gyroscope it goes left
-      break;
+        turn_R_360(right_strength, left_strength);  // TODO: Verify why in Gyroscope it goes left
+        break;
     case RIGHT:
-      turn_L_360(right_strength, left_strength);  // TODO: Verify why in Gyroscope it goes right
-      break;
-  }
+        turn_L_360(right_strength, left_strength);  // TODO: Verify why in Gyroscope it goes right
+        break;
+    }
 }
 
 String angle = "";
@@ -102,14 +107,14 @@ float cos_result;
 int cad;
 
 void mode_joystick(String* msg, bool* debug) {
-  advance_joystick(msg, &angle_rad, &sin_result, &cos_result, &strength, &cad);
+    advance_joystick(msg, &angle_rad, &sin_result, &cos_result, &strength, &cad);
 
-  if (*debug && Serial.availableForWrite() > 30) {
+    if (*debug && Serial.availableForWrite() > 30) {
 
-    Serial.print("sin:" + String(sin_result) + " cos:" + String(cos_result) + " str:" + String(strength) + " cad:" + String(cad)
-                 + " <--: " + String(left_strength) + " -->: " + String(right_strength));
-  }
-  else if (Serial.availableForWrite() > 30) { // Tells the server that Arduino is ready to receive a command
-    Serial.print("ok");
-  }
+        Serial.print("sin:" + String(sin_result) + " cos:" + String(cos_result) + " str:" + String(strength) + " cad:" + String(cad)
+            + " <--: " + String(left_strength) + " -->: " + String(right_strength));
+    }
+    else if (Serial.availableForWrite() > 30) { // Tells the server that Arduino is ready to receive a command
+        Serial.print("ok");
+    }
 }
