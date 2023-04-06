357a792e9a8dda3007bef7b8bce8c15ab659959b
LauriGui <laurianne.guindon@polymtl.ca>
2023-04-06 13:58:41 -0400

    Theads WORKgit add .git add .

--- Smol/Arduino/Smol_arduino/Smol_arduino.ino
+++ Smol/Arduino/Smol_arduino/Smol_arduino.ino
@@ -2,62 +2,68 @@
 #include "gyroscope.h"
 #include "joystick.h"
 #include "servomotor.h"
+#include <protothreads.h>  // https://www.digikey.com/en/maker/blogs/2022/how-to-write-multi-threaded-arduino-programs
 
 Servomotor cane;
+pt ptServo;
+pt ptMove;
 
-void setup(void) {
-  cane.init(8, 45, 90);
-  int i;
-  for (i = 4; i <= 7; i++)
-    pinMode(i, OUTPUT);
-  Serial.begin(9600);  //Set Baud Rate
-  // Serial.println("Run keyboard control");
+int servo(struct pt* pt) {
+  PT_BEGIN(pt);
+
+  //if (started) {
+  while (true) {
+    cane.start(1000);
+    PT_SLEEP(pt, 1);
+  }
+  //} else {
+  //  cane.stop(45);
+  //}
+
+  PT_END(pt);
 }
 
-void loop(void) {
-  bool debug = false;
-  String msg = "";
-  int mode = 1000;
-  bool started = false;
+int move(struct pt* pt) {
+  PT_BEGIN(pt);
 
-  //while (!Serial.available())
-  //  ; // TODO: is this really necessary?
+  while (true) {
+    bool debug = false;
+    String msg = "";
+    int mode = 1000;
 
-  if (started) {
-    cane.start(1000);
-  } else {
-    cane.stop(45);
-  }
-
-  if (Serial.available()) {
+    if (!Serial.available()) {
+      return;
+    }
+    
     msg = Serial.readString();
-
     mode = getValue(msg, ',', MODE_POSITION).toInt();
     debug = getValue(msg, ',', DEBUG_POSITION).toInt();
 
     switch (mode) {
       case MODE_JOYSTICK:
-        //int strength = getValue(msg, ',', STRENGTH_POSITION).toInt();
-        //if (strength == 0) {
-        if (msg == "1,x") {
-            started = false;
-        } else {
-            started = true;
-        }
         mode_joystick(&msg, &debug);
         break;
-
       case MODE_GYROSCOPE:
-        //char command = getValue(msg, ',', LETTER_POSITION)[0];
-        //if (command == -1 || command == 'x') {
-        if (msg == "1,x") {
-            started = false;
-        } else {
-            started = true;
-        }
-        cane.start(100);
         mode_gyroscope(&msg, &debug);
         break;
     }
   }
+
+  PT_END(pt);
+}
+
+void setup(void) {
+  PT_INIT(&ptServo);
+  PT_INIT(&ptMove);
+
+  cane.init(8, 45, 90);
+  int i;
+  for (i = 4; i <= 7; i++)
+    pinMode(i, OUTPUT);
+  Serial.begin(9600);  //Set Baud Rate
+}
+
+void loop(void) {
+  PT_SCHEDULE(servo(&ptServo));
+  PT_SCHEDULE(move(&ptMove));
 }
